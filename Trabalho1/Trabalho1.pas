9127edaf45618d4a72a6135d11466ba715f4f194
Joao Felipe Nicolaci Pimentel <joao@joao-R590.(none)>
2011-04-10 19:54:13 -0300

    Sun Apr 10 19:54:03 2011

--- Trabalho1/Trabalho1.pas
+++ Trabalho1/Trabalho1.pas
@@ -3,16 +3,22 @@ program Trabalho1;
 uses TestUnit, MatrizUnit, TestReducaoGaussUnit, TestMetodosIterativosUnit;
 
 var
-  Tamanho : integer;
-  Valor : real;
+  Tamanho, Iteracoes, RetornoIterativo : integer;
+  Precisao : real;
+  Escolha : integer;
   Coeficientes, CoeficientesTemp : TMatriz;
-  TermosIndependentes, TermosIndependentesTemp : TVetor;
+  TermosIndependentes, TermosIndependentesTemp, Resultado : TVetor;
+  rodando: boolean;
 
 procedure LerMatriz;
 var i, j: integer;
 begin
-  writeln('Digite o numero de incognitas');
-  readln(Tamanho);
+  Tamanho := 21;
+  while Tamanho > 20 do
+  begin
+    writeln('Digite o numero de incognitas');
+    readln(Tamanho);
+  end;
 
   for i := 1 to Tamanho do
   begin
@@ -27,9 +33,51 @@ begin
 end;
 
 
-
+procedure ExecutarIterativo(func: TIterativo);
+begin
+  writeln('Digite o numero máximo de iterações');
+  readln(Iteracoes);
+  writeln('Digite a precisão (ex 0.1)');
+  readln(Precisao);
+  RetornoIterativo := func(Resultado, NovoVetor('[0, 0, 0]'), CoeficientesTemp, TermosIndependentesTemp, Iteracoes, Tamanho, Tamanho, Precisao);
+  if RetornoIterativo = -1 then
+    writeln('Não é possível resolver, o sistema não satisfaz as condições suficientes de convergencia')
+  else begin
+    if RetornoIterativo = 0 then
+      writeln('O numero de iteracoes Máximo não foi suficiente para definir o resultado com a precisao definida')
+    else
+      writeln('A seguinte solucao foi encontrada com ', RetornoIterativo, ' iterações');
+      MostrarResultados(Resultado, Tamanho);
+  end;
+end;
 
 begin
   LerMatriz;
-  MostrarSistema(Coeficientes, TermosIndependentes, Tamanho, Tamanho);
+  rodando := true;
+  while rodando do
+  begin(
+    writeln;
+    writeln('Resolvendo o seguinte sistema');
+    MostrarSistema(Coeficientes, TermosIndependentes, Tamanho, Tamanho);
+    writeln;
+    writeln('Por que metodo você deseja resolver?');
+    writeln('1- Redução de Gauss');
+    writeln('2- Jacobi');
+    writeln('3- Gauss-Seidel');
+    writeln('4- Desejo escolher outro sistema');
+    writeln('5- Sair');
+    readln(Escolha);
+    //Esqueci como usar case e não estou com vontade de procurar agora
+    CoeficientesTemp := Coeficientes;
+    TermosIndependentesTemp := TermosIndependentes;
+    if Escolha = 1 then
+    begin
+      
+      Resultado := ResolucaoCompletaGauss(CoeficientesTemp, TermosIndependentesTemp, Tamanho, Tamanho);
+      MostrarResultados(Resultado, Tamanho);
+    end else if Escolha = 2 then 
+    begin
+      ExecutarIterativo(@jacobi);
+    end;
+  end;
 end.
