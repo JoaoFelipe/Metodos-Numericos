12bb02dffded9983a7c3a48711ad3cb081bef12c
Joao Felipe Nicolaci Pimentel <joao@joao-R590.(none)>
2011-04-10 19:30:23 -0300

    Sun Apr 10 19:30:23 2011

--- Trabalho1/ReducaoGaussUnit.pas
+++ Trabalho1/ReducaoGaussUnit.pas
@@ -5,9 +5,9 @@ interface
   uses MatrizUnit;
 
   function LinhaMultiplicador(matriz: TMatriz; Coluna, Linha, Linhas: integer): integer;
-  procedure ReducaoDeGauss(var Matriz: TMatriz; var Coeficientes: TVetor; Linhas, Colunas:integer);
-  function ResolucaoGauss(Matriz: TMatriz; Coeficientes: TVetor; Linhas:integer): TVetor;
-  function ResolucaoCompletaGauss(Matriz: TMatriz; Coeficientes: TVetor; Linhas, Colunas:integer): TVetor;
+  procedure ReducaoDeGauss(var Matriz: TMatriz; var TermosIndependentes: TVetor; Linhas, Colunas:integer);
+  function ResolucaoGauss(Matriz: TMatriz; TermosIndependentes: TVetor; Linhas:integer): TVetor;
+  function ResolucaoCompletaGauss(Matriz: TMatriz; TermosIndependentes: TVetor; Linhas, Colunas:integer): TVetor;
   
 
 implementation
@@ -31,7 +31,7 @@ begin
   LinhaMultiplicador := PosValor; 
 end;
 
-procedure ReducaoDeGauss(var Matriz: TMatriz; var Coeficientes: TVetor; Linhas, Colunas:integer);
+procedure ReducaoDeGauss(var Matriz: TMatriz; var TermosIndependentes: TVetor; Linhas, Colunas:integer);
 var i, j, k: integer;
 Multiplicador: real;
 begin
@@ -39,39 +39,39 @@ begin
   begin
     k := LinhaMultiplicador(Matriz, j, j, Linhas);
     Matriz := TrocarLinha(Matriz, j, k, j, Colunas); 
-    swap(Coeficientes[j], Coeficientes[k]);
+    swap(TermosIndependentes[j], TermosIndependentes[k]);
     for i := j + 1 to Linhas do
     begin
       Multiplicador := Matriz[i, j] / Matriz[j, j];
       Matriz[i, j] := 0;
       Matriz := OperarLinha(Matriz, i, j, Multiplicador, j + 1, Colunas);
-      Coeficientes[i] := Coeficientes[i] - Multiplicador*Coeficientes[j];
+      TermosIndependentes[i] := TermosIndependentes[i] - Multiplicador*TermosIndependentes[j];
     end;
   end;  
 end;
 
-function ResolucaoGauss(Matriz: TMatriz; Coeficientes: TVetor; Linhas:integer): TVetor;
+function ResolucaoGauss(Matriz: TMatriz; TermosIndependentes: TVetor; Linhas:integer): TVetor;
 var 
   retorno:TVetor;
   i, j: integer;
   soma :real;
 begin
-  retorno[Linhas] := Coeficientes[Linhas] / Matriz[Linhas, Linhas];
+  retorno[Linhas] := TermosIndependentes[Linhas] / Matriz[Linhas, Linhas];
   for i := Linhas - 1 downto 1 do
   begin
     soma := 0;
     for j := i + 1 to Linhas do
       soma := soma + Matriz[i,j]*retorno[j];
-    retorno[i] := (Coeficientes[i] - soma) / Matriz[i, i];
+    retorno[i] := (TermosIndependentes[i] - soma) / Matriz[i, i];
   end;
   ResolucaoGauss := retorno;
 end;
 
 
-function ResolucaoCompletaGauss(Matriz: TMatriz; Coeficientes: TVetor; Linhas, Colunas:integer): TVetor;
+function ResolucaoCompletaGauss(Matriz: TMatriz; TermosIndependentes: TVetor; Linhas, Colunas:integer): TVetor;
 begin
-  ReducaoDeGauss(Matriz, Coeficientes, Linhas, Colunas);
-  ResolucaoCompletaGauss := ResolucaoGauss(Matriz, Coeficientes, Linhas);
+  ReducaoDeGauss(Matriz, TermosIndependentes, Linhas, Colunas);
+  ResolucaoCompletaGauss := ResolucaoGauss(Matriz, TermosIndependentes, Linhas);
 end;
 
 end.
